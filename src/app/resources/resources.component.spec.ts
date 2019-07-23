7b849f9f79988f77ffae3aa3f27d65a8f7fb8da3
Andrew Cook <andrew.cook@microsoft.com>
2019-07-23 16:01:24 -0700

    Fixing tests and spelling.

--- src/app/resources/resources.component.spec.ts
+++ src/app/resources/resources.component.spec.ts
@@ -1,16 +1,24 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MatCardModule } from '@angular/material/card';
 import { ResourcesComponent } from './resources.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from '../home/home.component';
 import { ProgramsComponent } from '../programs/programs.component';
 
 describe('ResourcesComponent', () => {
+  const routes: Routes = [
+    { path: "", component: HomeComponent }
+  ];
+
   let component: ResourcesComponent;
   let fixture: ComponentFixture<ResourcesComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [MatCardModule, RouterModule.forRoot(routes)],
       declarations: [ResourcesComponent,
-        ProgramsComponent]
+        ProgramsComponent,
+        HomeComponent]
     })
     .compileComponents();
   }));
