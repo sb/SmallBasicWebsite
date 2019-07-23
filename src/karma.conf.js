98a7739ba0cccef8cf87c8d0f5a62ed997b7527e
Andrew Cook <andrew.cook@microsoft.com>
2019-07-23 16:58:08 -0700

    Testing different code coverage.

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -18,7 +18,7 @@ module.exports = function (config) {
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, '../coverage'),
-      reports: ['html', 'lcovonly', 'text-summary'],
+      reports: ['html', 'lcovonly', 'text-summary', 'cobertura'],
       fixWebpackSourcePaths: true
     },
     reporters: ['progress', 'kjhtml', 'junit'],
