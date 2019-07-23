d98accfa881d54b3bd2af38131746866f607cbf9
ianfjohnson <35199578+ianfjohnson@users.noreply.github.com>
2019-07-23 12:31:15 -0700

    Created programs component which displays on resources page (#33)

--- src/app/resources/resources.component.ts
+++ src/app/resources/resources.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-resources',
   templateUrl: './resources.component.html',
-  styleUrls: ['./resources.component.css']
+  styleUrls: ['./resources.component.css'],
 })
 export class ResourcesComponent implements OnInit {
     public topics = [
